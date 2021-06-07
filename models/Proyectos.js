202d2b24459b586178d6f9e2dbe41b2e65b4457c
jose Brusa <josebrusa@outlook.com>
2021-06-07 17:40:47 -0300

    add url unicas

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const db = require('../config/db')
 const slug = require('slug');
+const shortid = require('shortid');
+
 
 const Proyectos = db.define('proyectos', {
     id : {
@@ -15,7 +17,7 @@ const Proyectos = db.define('proyectos', {
         beforeCreate(proyecto) {
             const url = slug(proyecto.nombre).toLowerCase();
 
-            proyecto.url = url;
+            proyecto.url = `${url}-${shortid.generate()}`;
         },
     }
 });
