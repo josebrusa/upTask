fd13b38665bd62c63b63c0730dff7e0a138ded64
jose Brusa <josebrusa@outlook.com>
2021-06-11 18:59:21 -0300

    identificado chekc list por id

---
+++ public/js/modulos/tareas.js
@@ -0,0 +1,13 @@
+const tareas = document.querySelector('.listado-pendientes')
+
+if(tareas) {
+
+    tareas.addEventListener('click', e => {
+        if(e.target.classList.contains('fa-check-circle')){
+            const icono = e.target;
+            const idTarea = icono.parentElement.parentElement.dataset.tarea;
+        }
+    });
+}
+
+export default tareas;
